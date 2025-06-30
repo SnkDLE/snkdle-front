9d76e7bea918a196a36e232ca793675f82290ae3
Enzo ANGOT <81158245+Nianoute@users.noreply.github.com>
2025-06-30 22:39:35 +0200

    clear oublie

--- src/components/organism/index.js
+++ src/components/organism/index.js
@@ -1,7 +1,6 @@
-import TodoList from "./TodoList";
 import * as Image from "./Image";
 import * as Character from "./Character";
 import * as Attempt from "./Attempt";
 import * as Winner from "./Winner";
 
-export { TodoList, Image, Character, Attempt, Winner };
+export { Image, Character, Attempt, Winner };
